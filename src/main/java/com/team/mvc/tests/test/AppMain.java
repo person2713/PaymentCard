842891ef4c04b9711ca90146dd5d3ca03b61b4e8
Daolot <trebvit@gmail.com>
2017-03-26 18:56:11 +0400

    25.03

--- src/main/java/com/team/mvc/tests/test/AppMain.java
+++ src/main/java/com/team/mvc/tests/test/AppMain.java
@@ -2,7 +2,8 @@ package com.team.mvc.tests.test;
 
 import com.team.mvc.database.entities.Cities;
 import com.team.mvc.database.repositories.CitiesRepository;
-
+import com.team.mvc.tests.utils.HibernateSessionFactory;
+import org.hibernate.Session;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,12 +13,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppMain {
 
         public static void main(String[] args) {
-            AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-            ctx.scan("com.team.mvc.database.repositories");//This will load the configured components UserService, UserRepository,
+           /* AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+            ctx.scan("com.team.mvc");//This will load the configured components UserService, UserRepository,
             ctx.refresh();
-
             System.out.println(ctx);
-        //    UserService userService = ctx.getBean("userService", UserService.class);
+     /*   //    UserService userService = ctx.getBean("userService", UserService.class);
 
 
             CitiesRepository citiesRepository = ctx.getBean("citiesRepository", CitiesRepository.class);
@@ -26,11 +26,31 @@ public class AppMain {
 
             Cities citiesEntity = new Cities();
 
-            citiesEntity.setCityName("Pavlovsk");
+            System.out.println(citiesEntity.getCityName().toString());
 
-            citiesRepository.save(citiesEntity);
+            citiesRepository.save(citiesEntity);*/
 
 
 
-        }
-    }
+
+
+                    System.out.println("Hibernate tutorial");
+
+                    Session session = HibernateSessionFactory.getSessionFactory().openSession();
+
+                    session.beginTransaction();
+
+                    Cities cities = new Cities();
+
+                    cities.setCityName("Pavlovsk");
+
+                    session.save(cities);
+                    session.getTransaction().commit();
+
+                    session.close();
+                }
+            }
+
+
+
+
