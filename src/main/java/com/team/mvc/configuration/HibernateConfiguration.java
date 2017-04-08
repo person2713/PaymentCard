fc791ca359ef9e46ae22f4d530048b86a8d2cfcd
Nadein Nick <Nadein2713@mail.ru>
2017-04-09 02:41:54 +0300

    HibernateConnectionPool

--- src/main/java/com/team/mvc/configuration/HibernateConfiguration.java
+++ src/main/java/com/team/mvc/configuration/HibernateConfiguration.java
@@ -61,6 +61,11 @@ public class HibernateConfiguration {
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+
+        properties.put("hibernate.c3p0.min_size", environment.getRequiredProperty("hibernate.c3p0.min_size"));
+        properties.put("hibernate.c3p0.max_size", environment.getRequiredProperty("hibernate.c3p0.max_size"));
+        properties.put("hibernate.c3p0.timeout", environment.getRequiredProperty("hibernate.c3p0.timeout"));
+        properties.put("hibernate.c3p0.max_statements", environment.getRequiredProperty("hibernate.c3p0.max_statements"));
         return properties;
     }
 }
